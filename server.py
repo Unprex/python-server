435805530de680461fbc69e5c7588aaa1b45efdb
Unprex <bbrouant@sfr.fr>
2019-04-16 22:41:40 +0200

    Working template for server.

--- server.py
+++ server.py
@@ -1,13 +1,46 @@
-import logging
+# -*- coding: utf-8 -*-
+
 import socket
+import logging
+from terminal import Terminal
 from threading import Thread
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='[%(levelname)-5s] (%(threadName)-10s) %(message)s')
+
+class TerminalHandler(logging.StreamHandler):
+    def __init__(self, terminal):
+        logging.StreamHandler.__init__(self)
+        self.terminal = terminal
+        self.terminal.start()
+
+    def emit(self, record):
+        msg = self.format(record)
+        self.terminal.display(msg)
+
 
 MAX_HOST = 10
 
 serverIp = ("192.168.1.40", 23456)
+
+running = True
+
+
+def handle_input(term, text):
+    global running
+
+    text = text.strip().lower()
+    logging.debug("Command: %s", text)
+
+    if text == "stop":
+        running = False
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect(serverIp)
+        term.stop()
+
+
+term = Terminal(handle_input)
+logging.basicConfig(level=logging.DEBUG,
+                    format='[%(levelname)-5s] (%(threadName)-10s) %(message)s',
+                    handlers=[TerminalHandler(term)])  # StreamHandler()
 
 
 class Client(Thread):
@@ -21,23 +54,28 @@ class Client(Thread):
     def run(self):
         pass
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-socket.bind(("", serverIp[1]))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-socket.listen(MAX_HOST + 2)
+s.bind(("", serverIp[1]))
+
+s.listen(MAX_HOST + 2)
+
+hosts = []
+
 
 logging.debug("Listening for connections...")
-hosts = []
-n = 1
-while n > 0:
-    client, address = socket.accept()
+
+while True:
+    client, address = s.accept()
+    if not running:
+        break
     t = Client(client, address)
     hosts.append(t)
     t.start()
     hosts = [t for t in hosts if t.is_alive()]
-    n -= 1
 
 for h in hosts:
     h.join()
-socket.close()
+
+s.close()
--- terminal.py
+++ terminal.py
@@ -5,25 +5,40 @@ from threading import Thread
 
 
 class Terminal(Thread):
-    def __init__(self, ):
+    def __init__(self, callback=lambda *args: None, press=lambda *args: None):
         Thread.__init__(self)
+        self.callback = callback
+        self.press = press
         self.running = False
 
         self.prompt_text = ">"
-        self.lines_start = 1
         self.prompt_x = len(self.prompt_text)
+
+        self.lines = []
+        self.height = 0
+        self.width = 0
+        self.insert = True
 
     def run(self):
         self.running = True
         curses.wrapper(self.main_loop)
 
+    def stop(self):
+        self.running = False
+
+    def display(self, msg):
+        # Displays the message msg in the terminal
+        self.lines.append(msg)
+        if len(self.lines) > self.height - 1 and self.height > 0:
+            self.lines.pop(0)
+        self.draw()
+
     def main_loop(self, stdscr):
+        self.stdscr = stdscr
         k = 0
-        input_text = ""
+        self.input_text = ""
         input_x = self.prompt_x + len(self.input_text)
         cursor_x = self.prompt_x
-        lines = []
-        insert = True
 
         stdscr.keypad(False)  # TODO: Fix Numeric Keypad
 
@@ -31,51 +46,74 @@ class Terminal(Thread):
         stdscr.refresh()
 
         while self.running:
-            stdscr.clear()
-            height, width = stdscr.getmaxyx()
+            self.height, self.width = stdscr.getmaxyx()
 
             if k == curses.KEY_RIGHT:
                 cursor_x += 1
             elif k == curses.KEY_LEFT:
                 cursor_x -= 1
             elif k == curses.KEY_ENTER or k == 10 or k == 13:
-                lines.append(input_text)
-                while len(lines) > height - self.lines_start - 1:
-                    lines.pop(0)
-                input_text = ""
+                self.callback(self, self.input_text)
+                self.input_text = ""
             elif k == curses.KEY_BACKSPACE or k == 8:
                 if cursor_x > self.prompt_x:
                     pos = cursor_x - self.prompt_x
-                    input_text = input_text[:pos - 1] + input_text[pos:]
+                    self.input_text = self.input_text[:pos - 1] + \
+                        self.input_text[pos:]
                     cursor_x -= 1
             elif k == curses.KEY_DC or k == 127:
                 if cursor_x >= self.prompt_x:
                     pos = cursor_x - self.prompt_x
-                    input_text = input_text[:pos] + input_text[pos + 1:]
+                    self.input_text = self.input_text[:pos] + \
+                        self.input_text[pos + 1:]
             elif k == curses.KEY_HOME:
                 cursor_x = self.prompt_x
             elif k == curses.KEY_END:
                 cursor_x = input_x
             elif k == curses.KEY_IC:
-                insert = not insert
+                self.insert = not self.insert
             elif k == curses.KEY_EIC:
-                insert = False
+                self.insert = False
             else:
                 char = chr(k)
                 char_text = curses.keyname(k).decode("ascii")
                 if len(char_text) > 1 and char_text[0] == "^":
-                    pass
+                    self.press(self, char_text)
                 elif len(char_text) > 1 and char_text[0:4] == "ALT_":
-                    pass
+                    self.press(self, char_text)
                 elif char.isprintable():
                     pos = cursor_x - self.prompt_x
-                    if insert:
-                        input_text = input_text[:pos] + char + input_text[pos:]
+                    if self.insert:
+                        self.input_text = self.input_text[:pos] + \
+                            char + self.input_text[pos:]
                     else:
-                        input_text = input_text[:pos] + \
-                            char + input_text[pos + len(char):]
+                        self.input_text = self.input_text[:pos] + \
+                            char + self.input_text[pos + len(char):]
                     cursor_x += len(char)
 
-            stdscr.move(height - 1, cursor_x)
-            stdscr.refresh()
-            k = stdscr.getch()
+            input_x = self.prompt_x + len(self.input_text)
+            cursor_x = max(self.prompt_x, cursor_x)
+            cursor_x = min(self.width - 1, input_x, cursor_x)
+
+            stdscr.move(self.height - 1, cursor_x)
+            self.draw()
+            if self.running:
+                k = stdscr.getch()
+
+    def draw(self):
+        self.stdscr.clear()
+        self.height, self.width = self.stdscr.getmaxyx()
+
+        for i, l in enumerate(self.lines):
+            self.stdscr.addstr(i, 0, l)
+
+        self.stdscr.addstr(self.height - 1, 0,
+                           self.prompt_text + self.input_text)
+        self.stdscr.refresh()
+
+
+if __name__ == "__main__":
+    def callback(term, input):
+        term.display(input)
+
+    Terminal(callback)
