d57138ee1bf2cffc92fcb50de9f57dc46fd8fff8
Bastien Brouant <bastien@brouant.fr>
2019-10-03 22:46:12 +0200

    Updated client with 'select' module

--- client.py
+++ client.py
@@ -1,85 +1,229 @@
-import socket
+# -*- coding: utf-8 -*-
+
+import select
+import socket as sk
 import logging
-import common
+import queue
+
+from threading import Thread
+
+# import common
 from terminal import Terminal, TerminalHandler
-from threading import Thread, Event
+
+commands = {"stop": "stop", "ping": "ping"}
 
 
+def twosComp(value, bits):
+    """ Compute the 2's complement of int value. """
+    if (value & (1 << (bits - 1))) != 0:
+        value = value - (1 << bits)
+    return value
+
+
+LOOP_TIME = 0.1  # s
 serverIp = ("localhost", 23456)
 
-event = Event()
-input_text = ""
+
+class NoDataError(Exception):
+    """ When the client doesn't receive any data. """
+
+    def __init__(self, where):
+        self.where = where
+
+    def __str__(self):
+        return "No data received (" + str(self.where) + ")."
 
 
-def handle_input(term, text):
-    global event, input_text
+class Client(Thread):  # (common.Client):
+    def __init__(self, socket, address, term, inputQueue):
+        Thread.__init__(self)
+        self.socket = socket
+        self.address = address
+        self.currentData = bytearray()
+        self.dataQueue = queue.Queue()
+        self.taskCount = 0
 
-    text = text.strip().lower()
-    logging.debug("Command: %s", text)
-
-    input_text = text
-    event.set()
-    event.clear()
-
-
-class Client(common.Client):
-    def __init__(self, socket, address, term, event):
-        common.Client.__init__(self, socket, address)
         self.term = term
-        self.event = event
-        self.listen_loop = Thread(target=self._listen_loop)
-        self.running = False
+        self.inputQueue = inputQueue
+        # self.listen_loop = Thread(target=self._listen_loop)
+        # common.Client.__init__(self, socket, address)
+        self._connected()
 
     def run(self):
-        global input_text
-
-        self.running = True
+        """ Executed when the thread starts. """
         try:
-            self.listen_loop.start()
-
+            # Main loop.
             while True:
-                self.event.wait()
-                if input_text == "stop":
-                    self.running = False
-                    break
-                elif input_text == "ping":
-                    self._send(1, b"Test")
+                # Loops continuously TODO: select Queue
+
+                # User input loop.
+                try:
+                    inputText = self.inputQueue.get_nowait()
+                except queue.Empty:
+                    pass  # If there isn't any user input.
+                else:
+                    # TODO: Process input text
+                    if inputText == commands["stop"]:
+                        self.inputQueue.task_done()
+                        break
+                    elif inputText == commands["ping"]:
+                        self.socket.send(b"\x05\x01Test")
+
+                    self.inputQueue.task_done()
+
+                # Data receiving loop.
+                try:
+                    data = self.dataQueue.get_nowait()
+                except queue.Empty:
+                    pass  # If there isn't any packet received.
+                else:
+                    self.taskCount += 1
+                    if data is None:
+                        raise NoDataError("Packet")
+                    self.currentData += bytearray(data)
+                    self._recv()  # TODO : Check if all data is used.
+                    while self.taskCount > 0:
+                        self.dataQueue.task_done()
+                        self.taskCount -= 1
+
+        except NoDataError:  # If the server disconnects.
+            logging.warning("No data received.")
         except Exception:
-            logging.exception("Exception in run")
-        logging.info("Disconnected from %s, %s", self.address, self.running)
-        self.socket.shutdown(socket.SHUT_WR)  # TODO: end recv without server
-        self.listen_loop.join()
+            logging.exception("Exception in run.")
+        while self.taskCount > 0:
+            self.dataQueue.task_done()
+            self.taskCount -= 1
+        self._disconnected()
 
     def _connected(self):
-        logging.info("Connected to %s", self.address)
+        logging.info("Connected to %s.", self.address)
 
     def _disconnected(self):
-        logging.error("Disconnected from %s", self.address)
+        logging.info("Disconnected from %s.", self.address)
+
+    def _recv(self):
+        # Read packet info and data.
+        left, _ = self._readVarInt(1)  # Length
+        if left <= 0:
+            logging.warning("Received packet of length %s.", left)
+            while len(self.currentData) > 0 and self.currentData[0] == 0:
+                # Removing b"\x00" bytes (assuming erroneous data).
+                self.currentData.pop(0)
+            return
+        packetId, length = self._readVarInt(1)
+
+        logging.debug("Received packet %s of size %s.", packetId, left)
+        left -= length
+        assert left >= 0
+
+        while len(self.currentData) < left:
+            data = self.dataQueue.get()
+            self.taskCount += 1
+            if data is None:
+                raise NoDataError("Byte")
+            self.currentData += bytearray(data)
+
+        byte = self.currentData[:left]
+        self.currentData = self.currentData[left:]
+
+        logging.debug("Data: %s.", bytes(byte))
+
+    def _readVarInt(self, size):
+        """ Returns a VarInt value and its size from the currentData. """
+        value, i = 0, 0
+        while True:
+            while len(self.currentData) == 0:
+                data = self.dataQueue.get()
+                self.taskCount += 1
+                if data is None:
+                    raise NoDataError("VarInt")
+                self.currentData += bytearray(data)
+
+            byte = self.currentData.pop(0)
+            value |= (byte & 0x7F) << 7 * i
+            i += 1
+            if not byte & 0x80:
+                break
+        if i > 5 * size:
+            raise RuntimeError("VarInt longer than expected.")
+        return twosComp(value, 32 * size), i
 
 
-def main(term, event):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def main(term, inputQueue):
+    socket = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
 
-    logging.debug("Connecting to server...")
+    logging.info("Connecting to server...")
     try:
-        s.connect(serverIp)
+        socket.connect(serverIp)
     except (ConnectionAbortedError,
             ConnectionRefusedError,
             ConnectionResetError):
-        logging.error("Connection failed")
+        logging.error("Connection failed.")
     else:
-        t = Client(s, serverIp, term, event)
-        t.run()  # t.start() if Thread
+        client = Client(socket, serverIp, term, inputQueue)
+        client.start()
 
-    term.stop()
-    s.close()
+        outputs = []
+
+        # Loops when the socket is ready or every LOOP_TIME seconds.
+        while client.isAlive():
+            readable, writable, exceptional = select.select(
+                [socket], outputs, [socket], LOOP_TIME)
+
+            # When receiving something.
+            for s in readable:
+                try:
+                    data = s.recv(1024)
+                except (ConnectionAbortedError,
+                        ConnectionRefusedError,
+                        ConnectionResetError):
+                    logging.warning("Connection failed.")
+                    data = None
+                if data:  # If data is None: the server disconnected.
+                    client.dataQueue.put(data)
+                    # if s not in outputs:  # To send data.
+                    #     outputs.append(s)
+                else:
+                    client.dataQueue.put(None)
+                    if s in outputs:
+                        outputs.remove(s)
+
+            # When ready to send something.
+            for s in writable:
+                logging.debug("Writable.")
+
+            # When encountering an exception.
+            for s in exceptional:
+                client.dataQueue.put(None)
+                if s in outputs:
+                    outputs.remove(s)
+                logging.warning("Socket exception.")
+
+    socket.close()
 
 
 if __name__ == "__main__":
-    term = Terminal(handle_input)
+    # Setting up "graphics".
+
+    inputQueue = queue.Queue()
+
+    def handleInput(term, text):
+        """ Called by the terminal, forwards user input to client. """
+        text = text.strip().lower()
+        logging.debug("Command: %s", text)
+        inputQueue.put(text)
+
+    term = Terminal(handleInput)
+
     logging.basicConfig(
         level=logging.DEBUG,
         format='[%(levelname)-5s] (%(threadName)-10s) %(message)s',
         handlers=[TerminalHandler(term)])  # logging.StreamHandler()
 
-    main(term, event)
+    # Main program.
+    try:
+        main(term, inputQueue)
+    except Exception:
+        logging.exception("Exception in \"main\".")
+
+    term.stop()
